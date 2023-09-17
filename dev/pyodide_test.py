131332e7a7a375208944f3aef46aac9e5524a247
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2023-09-17 17:23:25 +0200

    updated content

--- dev/pyodide_test.py
+++
@@ -1,4 +0,0 @@
-import micropip
-await micropip.install('bw2io==0.9.dev21', keep_going = True)
-await micropip.install('bw2calc==2.0.dev14', keep_going = True)
-await micropip.install('bw2data==4.0.dev25', keep_going = True)
